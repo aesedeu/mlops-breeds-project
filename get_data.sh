1404b45a1b2f29b94b0198319a0cf67739b34c14
Eugene Chernov <ev7080@yandex.ru>
2025-11-26 22:06:50 +0300

    fix download data

--- get_data.sh
+++ get_data.sh
@@ -2,4 +2,6 @@
 source .venv/bin/activate
 gdown "https://drive.google.com/uc?id=1SyrHRYp3IRZ7b-vPOKJNJWn7hj97gFWa" -O breeds.zip
 unzip breeds.zip -d data
+cd data && mv data/* .
+rmdir data
 rm breeds.zip
